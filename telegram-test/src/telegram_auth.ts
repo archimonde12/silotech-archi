cf3d8cbb67992fb86cc9124104070bb329aa9e80
archimonde12 <>
2021-01-29 10:09:57 +0700

    fix

--- telegram-test/src/telegram_auth.ts
+++ telegram-test/src/telegram_auth.ts
@@ -2,10 +2,14 @@ import { createHmac } from "crypto"
 import { BOT_TOKEN } from "./config"
 import { TelegramUserData } from "./models/TelegramUserData"
 import { createDataCheckStringFromUserData, createSecretKeyFromBotToken } from "./ulti"
-
+/**
+ * 
+ * @param _botToken token of bot telegram
+ * @param _userData data of user after sign in the telegram app
+ * @link Formula hash at: https://core.telegram.org/widgets/login
+ * @link code example: https://gist.github.com/anonymous/6516521b1fb3b464534fbc30ea3573c2
+ */
 const checkHash = (_botToken: string, _userData: TelegramUserData) => {
-    //Formula hash at: https://core.telegram.org/widgets/login
-    // Sample PHP code for checking authorization and receiving data: https://gist.github.com/anonymous/6516521b1fb3b464534fbc30ea3573c2
     const checkHash = _userData.hash
     const secret_key = createSecretKeyFromBotToken(BOT_TOKEN)
     const data_check_string = createDataCheckStringFromUserData(_userData)
@@ -15,7 +19,6 @@ const checkHash = (_botToken: string, _userData: TelegramUserData) => {
     if (hash !== checkHash) {
         throw new Error("This message not be sent by Telegram")
     }
-
     const now = new Date().getTime()
     const nowInSecond = now / 1000
     const SECONDS_OF_ONE_DAY = 24 * 60 * 60 //Follow PHP Sample code
@@ -23,7 +26,6 @@ const checkHash = (_botToken: string, _userData: TelegramUserData) => {
     if (nowInSecond - _userData.auth_date > SECONDS_OF_ONE_DAY) {
         throw new Error("This token is out of date")
     }
-
     return
 }
 
